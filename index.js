cbf21c7b057a355794e4938fbcfb0f193d183a9d
allevo <tomallevi@gmail.com>
2015-03-18 17:31:52 +0100

    Use express.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 'use strict';
 
-
-var http = require('http');
-
 var PORT = 3000;
 
+var express = require('express');
+var app = express();
+
+function pippoHandler(req, res) {
+  console.log(req.url, req.query, req.body);
+  res.status(200).json({res: 'ok'});
+}
+
+app.get('/pippo', pippoHandler);
+app.post('/pippo', pippoHandler);
+
 var listenCallback = console.log.bind(console, 'listen to %d', PORT);
-
-var server = http.createServer(function(req, res) {
-  console.log('Request %s is coming', req.url);
-
-  res.end('ok\n');
-});
-server.listen(PORT, listenCallback);
+app.listen(PORT, listenCallback);
